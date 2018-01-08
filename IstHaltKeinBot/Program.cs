9ae154961c3f77c8b871f5b7b71d74de27e3d6aa
Maik Riedlsperger <maik.riedlsperger@gmail.com>
2018-01-08 11:31:05 +0100

    Basic code

--- IstHaltKeinBot/Program.cs
+++ IstHaltKeinBot/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tweetinvi;
 
 namespace IstHaltKeinBot
 {
@@ -10,6 +11,10 @@ namespace IstHaltKeinBot
     {
         static void Main(string[] args)
         {
+            Auth.SetUserCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
+            String game1 = "";
+            String game2 = "";
+            Tweet.PublishTweet(game1 + " ist halt kein " + game2);
         }
     }
 }
